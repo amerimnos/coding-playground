49303653b08804c6cd6f9b5e18b3aed7d47b2b78
yanggitak <gt.yang@pivot-inc.com>
2022-10-23 20:52:49 +0900

    Debounce(디바운스) 예제

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-let list = {
-    value: 1,
-    next: {
-        value: 2,
-        next: {
-            value: 3,
-            next: {
-                value: 4,
-                next: null,
-            },
-        },
-    },
-};
 
-function printReverseList(list) {
-    debugger;
-    if (list.next) {
-        printReverseList(list.next);
-    }
-   
-    console.log(list.value);
+let temp = '';
+let test = debounce(f, 1000);
+
+function debounce(func, ms) {
+    let timeout;
+    return function (...arguments) {
+        temp += arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, arguments), ms);
+    };
 }
 
-printReverseList(list);
+function f() {
+    alert(temp);
+}
+
+setTimeout(() => { test('a'); }, 10);   // 무시(1000ms 이내에 아래 함수가 실행)
+setTimeout(() => { test('b'); }, 100);  // 발생(100ms 이후 부터 1000ms 이내에 실행되는 함수 없음)
+setTimeout(() => { test('c'); }, 1200); // 무시(1000ms 이내에 아래 함수가 실행)
+setTimeout(() => { test('d'); }, 1400); // 무시(1000ms 이내에 아래 함수가 실행)
+setTimeout(() => { test('e'); }, 2500); // 발생(2500ms 이후 부터 1000ms 이내에 실행되는 함수 없음)
