68a25a2e7aea3998d26941cc50bd92c2d896df4b
yanggitak <gt.yang@pivot-inc.com>
2022-11-05 15:01:55 +0900

    함수 바인딩과 화살표 함수에 따라 this에 대하여... 2
    Signed-off-by: yanggitak <gt.yang@pivot-inc.com>

--- script.js
+++ script.js
@@ -1,48 +1,49 @@
 function askPassword(ok, fail) {
-    let password = prompt("비밀번호를 입력해주세요.", '');
-    if (password == "rockstar") ok();
-    else fail();
+    password = null;
+
+    setTimeout(() => {
+
+        password = prompt("비밀번호를 입력해주세요.", '');
+
+        setTimeout(() => {
+            user = {
+                login() {
+                    1 + 1
+                }
+            };
+
+            setTimeout(() => {
+                if (password == "rockstar") ok();
+                else fail();
+            }, 500);
+
+        }, 500);
+
+    }, 500);
 }
 
 let user = {
     name: 'John',
 
-    loginOk() {
-        console.log(this, 'asdfasdf');
-        console.log(`${this.name}님이 로그인하였습니다.`);
-    },
-
-    loginFail() {
-        console.log(this, 'asdfasdf');
-        console.log(`${this.name}님이 로그인에 실패하였습니다.`);
-    },
-
+    login(result) {
+        alert(this.name + (result ? ' 로그인 성공' : ' 로그인 실패'));
+    }
 };
 
-console.log(user.loginOk, 'user.loginOk')
 
-
-
-
-askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
 /**
- * 위에 녀석은 바인딩 되어 this 값이 존재함.. 해당 컨텍스트를 유지하지... user로...
+ * 방식1. 전달 받은 user는 호출 된 시점부터 무조건 고정됨...
  */
 
+// askPassword(user.login.bind(user, true), user.login.bind(user, false));
 
 
- askPassword(user.loginOk, user.loginFail);
+
+
 /**
- * 위에 녀석은 함수 안에 this가 undefined라서 에러 발생.
+ * 방식2. 심플함, 문제는 입력 받은 후 함수 호출 전에 user가 바뀌면 결과도 바뀌게됨...
  */
-
-
-
 askPassword(
-    () => user.loginOk(),
-    () => user.loginFail()
+    () => user.login(true),
+    () => user.login(false)
 );
-/**
- * 왜 화살표 함수는 메서뜨 함수 안에 있는 this를 인식하는거지??
- * 화살표 함수를 사용하면 현재 컨텍스트를 잃지 않아 편리...
- */
