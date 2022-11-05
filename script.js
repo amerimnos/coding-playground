e4337fc4ced286b5f5df56fcf80be8cf03fa672b
yanggitak <gt.yang@pivot-inc.com>
2022-11-05 14:32:49 +0900

    함수 바인딩과 화살표 함수에 따라 this에 대하여...

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-
-let temp = '';
-let test = debounce(f, 1000);
-
-function debounce(func, ms) {
-    let timeout;
-    return function (...arguments) {
-        temp += arguments;
-        clearTimeout(timeout);
-        timeout = setTimeout(() => func.apply(this, arguments), ms);
-    };
+function askPassword(ok, fail) {
+    let password = prompt("비밀번호를 입력해주세요.", '');
+    if (password == "rockstar") ok();
+    else fail();
 }
 
-function f() {
-    alert(temp);
-}
+let user = {
+    name: 'John',
 
-setTimeout(() => { test('a'); }, 10);   // 무시(1000ms 이내에 아래 함수가 실행)
-setTimeout(() => { test('b'); }, 100);  // 발생(100ms 이후 부터 1000ms 이내에 실행되는 함수 없음)
-setTimeout(() => { test('c'); }, 1200); // 무시(1000ms 이내에 아래 함수가 실행)
-setTimeout(() => { test('d'); }, 1400); // 무시(1000ms 이내에 아래 함수가 실행)
-setTimeout(() => { test('e'); }, 2500); // 발생(2500ms 이후 부터 1000ms 이내에 실행되는 함수 없음)
+    loginOk() {
+        console.log(this, 'asdfasdf');
+        console.log(`${this.name}님이 로그인하였습니다.`);
+    },
+
+    loginFail() {
+        console.log(this, 'asdfasdf');
+        console.log(`${this.name}님이 로그인에 실패하였습니다.`);
+    },
+
+};
+
+console.log(user.loginOk, 'user.loginOk')
+
+
+
+
+askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+/**
+ * 위에 녀석은 바인딩 되어 this 값이 존재함.. 해당 컨텍스트를 유지하지... user로...
+ */
+
+
+
+ askPassword(user.loginOk, user.loginFail);
+/**
+ * 위에 녀석은 함수 안에 this가 undefined라서 에러 발생.
+ */
+
+
+
+askPassword(
+    () => user.loginOk(),
+    () => user.loginFail()
+);
+/**
+ * 왜 화살표 함수는 메서뜨 함수 안에 있는 this를 인식하는거지??
+ * 화살표 함수를 사용하면 현재 컨텍스트를 잃지 않아 편리...
+ */
