1c94f2b94cdbe854edfd718d00a8c215a94d368a
yanggitak <anthimus3@gmail.com>
2022-12-31 23:46:15 +0900

    swiper 인스턴스 컨트롤
    세로 모드에서 시작해도 가로 모드에서 시작해도 문제 없음.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let TEST = {
     instanceOfSwiper: null,
 }
 
-TEST.instanceOfSwiper = new Swiper(".mySwiper", {
+/* var test1 = new Swiper(".mySwiper", {
     width: 198,
     spaceBetween: 6,
     touchReleaseOnEdges: true,
@@ -27,19 +27,26 @@ TEST.instanceOfSwiper = new Swiper(".mySwiper", {
         },
         resize: function () {
             window.addEventListener('orientationchange', () => {
-                console.log('orientationchange...')
+                console.log('orientationchange...origin');
                 setTimeout(() => {
-                    this.destroy();
+                    test1.destroy();
                     controlZeroDaySwiper();
                 }, 30);
-            }, { once: true }
-            );
+            }, { once: true });
         },
     },
+}); */
+
+controlZeroDaySwiper();
+
+window.addEventListener("orientationchange", function () {
+    setTimeout(() => {
+        TEST.instanceOfSwiper.destroy();
+        controlZeroDaySwiper(); 
+    }, 30);
 });
 
 function controlZeroDaySwiper() {
-
     TEST.instanceOfSwiper = new Swiper(".mySwiper", {
         width: 198,
         spaceBetween: 6,
@@ -49,34 +56,31 @@ function controlZeroDaySwiper() {
         on: {
             beforeInit: function () {
 
-                const numOfslidesToCopy = TEST.numOfSlidesToZeroDay + 3,
-                    SumOfMarginsOnBothSidesOfSwiper = 40,
+                try {
+                    this.$el[0].querySelectorAll('.swiper-slide-duplicate, .swiper-notification').forEach(element => {
+                        element.remove();
+                    })
+                } catch (error) { console.error(error) }
+
+                console.log(TEST.numOfSlidesToZeroDay, 'TEST.numOfSlidesToZeroDay')
+                TEST.numOfSlidesToZeroDay = this.el.querySelectorAll('.swiper-slide').length;
+
+                const SumOfMarginsOnBothSidesOfSwiper = 40,
+                    numOfslidesToCopy = TEST.numOfSlidesToZeroDay + 1,
                     widthOfSlide = this.params.width - SumOfMarginsOnBothSidesOfSwiper;
 
                 if (TEST.numOfSlidesToZeroDay * (widthOfSlide + this.params.spaceBetween) <= window.innerWidth - SumOfMarginsOnBothSidesOfSwiper) {
-
-                        try {
-                            this.$el[0].querySelectorAll('.swiper-slide-duplicate, .swiper-notification').forEach(element => {
-                                element.remove();
-                            })
-                        } catch (error) { }
-                        this.update();
-                        this.allowTouchMove = false;
+                    console.log("기기 해상도가 더 큼")
+                    this.allowTouchMove = false;
                 } else {
+                    console.log("기기 해상도가 더 작음")
                     this.params.loop = true;
                     this.params.loopAdditionalSlides = numOfslidesToCopy;
-                    this.update();
                 }
-            },
-            resize: function () {
-                window.addEventListener('orientationchange', () => {
-                    setTimeout(() => {
-                        TEST.instanceOfSwiper.init();
-                        TEST.instanceOfSwiper.destroy();
-                        controlZeroDaySwiper();
-                    }, 30);
-                });
+                // debugger;
             },
         },
     });
+
+    console.log(TEST.instanceOfSwiper, 'TEST.instanceOfSwiper');
 }
