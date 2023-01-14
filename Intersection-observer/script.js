878766189870057d15b5b77bddd0bd5d2353fcba
yanggitak <anthimus3@gmail.com>
2023-01-14 21:53:16 +0900

    getBoundingClientRect와 Intersection Observer API를 활용한 scroll 모션 제어
    리팩토링 - 컴포넌트 화

--- Intersection-observer/script.js
+++ Intersection-observer/script.js
@@ -3,20 +3,20 @@ window.addEventListener("scroll", () => {
 })
 
 /**
+ * @description scroll에 따른 class 추가
  * @param target string : 예시) .box or #box
  * @param NameOfCustomCSS string
  * @param ValueOfTransY number
  */
-
 function activeAnimationTransY(target, NameOfCustomCSS, ValueOfTransY) {
-    var scheduledAnimationFrame = false;
-    var boxElements = document.querySelectorAll(target);
+    var _scheduledAnimationFrame = false;
+    var _boxElements = document.querySelectorAll(target);
 
-    if (scheduledAnimationFrame) return;
+    if (_scheduledAnimationFrame) return;
 
-    scheduledAnimationFrame = true;
+    _scheduledAnimationFrame = true;
     requestAnimationFrame(function () {
-        boxElements.forEach(function (el) {
+        _boxElements.forEach(function (el) {
 
             if (getElemOfViewportHeightRatio(el) < 0 || getElemOfViewportHeightRatio(el) > 1) return;
 
@@ -29,23 +29,21 @@ function activeAnimationTransY(target, NameOfCustomCSS, ValueOfTransY) {
                 el.classList.remove('active')
             }
         })
-        scheduledAnimationFrame = false;
+        _scheduledAnimationFrame = false;
     })
-}
 
-/**
- * 해당 요소의 viewport height의 위치 비율.
- */
-function getElemOfViewportHeightRatio(el) {
-    var _ratio = 0;
+    /**
+     * @description 해당 요소들의 scroll ratio
+     */
+    function getElemOfViewportHeightRatio(el) {
+        var _ratio = 0;
 
-    //if(el.getBoundingClientRect().top > window.innerHeight || el.getBoundingClientRect().bottom < 0) return _ratio;
+        if (_ratio >= 0 && _ratio <= 1) {
+            _ratio = el.getBoundingClientRect().bottom / (window.innerHeight + el.getBoundingClientRect().height)
+        }
+        if (_ratio < 0) _ratio = 0;
+        if (_ratio > 1) _ratio = 1;
 
-    if (_ratio >= 0 && _ratio <= 1) {
-        _ratio = el.getBoundingClientRect().bottom / (window.innerHeight + el.getBoundingClientRect().height)
+        return _ratio;
     }
-    if (_ratio < 0) _ratio = 0;
-    if (_ratio > 1) _ratio = 1;
-
-    return _ratio;
 }
