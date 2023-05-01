fe32e025613d2b23e0269f02f54d549c1344f346
yanggitak <anthimus3@gmail.com>
2023-05-02 00:07:01 +0900

    cache-control - apach 웹서버 활용 테스트

---
+++ cache-control/script.js
@@ -0,0 +1,15 @@
+function setTest() {
+    return 1 + 1;
+}
+function setTest() {
+    return 1 + 1;
+}
+function setTest() {
+    return 1 + 1;
+}
+function setTest() {
+    return 1 + 1;
+}
+function setTest() {
+    return 1 + 1;
+}
