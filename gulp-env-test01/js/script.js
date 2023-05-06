5e10d3e434ec986d04d727d017ba1f7992efbdcd
yanggitak <anthimus3@gmail.com>
2023-05-06 23:39:17 +0900

    gulp 환경 구축

---
+++ gulp-env-test01/js/script.js
@@ -0,0 +1,16 @@
+
+var a = 1;
+var b = 2;
+var c = 3;
+
+
+/**
+ * test 123
+ */
+function test123() {
+    return a + b;
+}
+
+console.log('test123()', test123() + 2);
+
+console.log('4441231231231231231234');
