ff695d50bd3bb7a7b54c042794047c0186b9c3e5
yanggitak <anthimus3@gmail.com>
2023-05-07 15:13:07 +0900

    css, js tree shaking 적용여부 확인중

--- gulp-env-test01/gulpfile.js
+++ gulp-env-test01/gulpfile.js
@@ -4,6 +4,7 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const cleanCss = require('gulp-clean-css');
 const del = require('del');
+const purify = require('gulp-purifycss');
 const xto6 = require('gulp-xto6');
 
 
@@ -15,22 +16,23 @@ const clean = () => {
 };
 
 /**
- * @description 모든 파일과 폴더를 dist 폴더로 복사
+ * @description 모든 파일과 폴더를 dist 폴더로 복사(!파일 제외)
  */
 const copyFiles = () => {
-    return gulp.src([
-        './**/*',
-        '!node_modules/**',
-        '!gulpfile.js',
-        '!package-lock.json',
-        '!package.json',
-        '!gitignore.json'
-    ])
-        .pipe(gulp.dest('dist'));
+    return gulp
+        .src([
+            './**/*',
+            '!node_modules/**',
+            '!gulpfile.js',
+            '!package-lock.json',
+            '!package.json',
+            '!gitignore.json'
+        ])
+        .pipe(gulp.dest('./dist'));
 };
 
 /**
- * @description CSS 축소화/tree shaking
+ * @description CSS 축소화
  */
 const css = () => {
     return gulp
@@ -40,12 +42,24 @@ const css = () => {
 };
 
 /**
+ * @description CSS tree shaking
+ * @TODO 작동되지 않아 확인 필요
+ */
+const purifyCss = () => {
+    return gulp
+        .src('./dist/css/style.css')
+        .pipe(purify(['./dist/js/**/*.js', './dist/**/*.html']))
+        .pipe(gulp.dest('./dist'));
+};
+
+
+/**
  * @description JS es5->es6 변환/난독화/축소화/tree shaking
  */
 const js = () => {
     return gulp.src('./dist/js/**/*.js')
-        //.pipe(xto6()) TODO : 굳이 es5를 es6로 변한할 필요가 있을까? xtos는 작동하나 관리가 오랬동안 안되고 있음.
-        .pipe(uglify()) // TODO : 기본 옵션으로 tree shaking 같은 코드가 활성화되어 있으나 웹팩 같은 환경이 아니기 때문에 잘 적용은 안되는 것으로 보임.
+        //.pipe(xto6()) TODO : es5를 es6로 변한할 필요가 있을까?
+        .pipe(uglify()) // TODO : 기본 옵션으로 tree shaking 관련 코드가 활성화되어 있으나 정적 분석을 지원하지 않아서인지 작동하지 않음.
         .pipe(gulp.dest('./dist/js/'));
 };
 
@@ -60,7 +74,8 @@ const js = () => {
 const build = gulp.series(
     clean,
     copyFiles,
-    gulp.parallel(css, js)
+    gulp.parallel(css, js),
+    purifyCss
 );
 
 gulp.task('build', build);
--- gulp-env-test01/js/script.js
+++ gulp-env-test01/js/script.js
@@ -1,8 +1,7 @@
 
 var a = 1;
 var b = 2;
-var c = 3;
-
+const c = 3;
 
 /**
  * test 123
@@ -12,5 +11,8 @@ function test123() {
 }
 
 console.log('test123()', test123() + 2);
-
 console.log('4441231231231231231234');
+
+setTimeout(function test321() {
+    c = test123 + 1;
+}, 1000);
