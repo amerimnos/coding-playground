33c70fb8b7f98453d60d7336d4c77fd57efc0774
yanggitak <anthimus3@gmail.com>
2023-05-17 23:52:03 +0900

    버튼 눌렀을때 svg 모션 - 버그 수정
    - 반대 방향으로 빠르게 버튼 번갈아 누르면 모션이 자연스럽지 않아서 모션이 끝날때까지 작동안되게 처리

--- svg-animation01/script.js
+++ svg-animation01/script.js
@@ -3,17 +3,29 @@ const curvBtn = document.querySelector('.curv-btn');
 const flatAnime = document.querySelector('animate.flat');
 const curvAnime = document.querySelector('animate.curv');
 const svg = document.querySelector('.svg');
+let isActive = false;
 
 flatBtn.addEventListener("pointerdown", function () {
     if (svg.dataset.mode === flatBtn.dataset.mode) return;
-    svg.dataset.mode = flatBtn.dataset.mode;
-    curvAnime.beginElement();
+    if (isActive === false) {
+        isActive = true
+        svg.dataset.mode = flatBtn.dataset.mode;
+        curvAnime.beginElement();
+        setTimeout(() => {
+            isActive = false;
+        }, 500);
+    }
 })
 
 curvBtn.addEventListener("pointerdown", function () {
     if (svg.dataset.mode === curvBtn.dataset.mode) return;
-    svg.dataset.mode = curvBtn.dataset.mode;
-    flatAnime.beginElement();
+    if (isActive === false) {
+        isActive = true
+        svg.dataset.mode = curvBtn.dataset.mode;
+        flatAnime.beginElement();
+        setTimeout(() => {
+            isActive = false;
+        }, 500);
+    }
 })
-
 
